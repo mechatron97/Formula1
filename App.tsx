539f85ad82c2eb272a3e4e0ffc9b2bc703831624
Sriharsh Sai <sriharsh1997@gmail.com>
2024-05-08 18:14:23 -0400

    second

--- App.tsx
+++ App.tsx
@@ -1,30 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import racesResponse from './assets/data/races.json';
-import { Entypo } from '@expo/vector-icons';
-import { Colors } from './src/Constants/Colors';
 
 const races = racesResponse.data.races.response;
 
-function RaceListItem({item}: {item: (typeof races)[0]}) {
-  return(
-    <View style={styles.itemContainer}>
-      {/* <Text style={styles.date}>{item.date}</Text> */}
-      <View style={styles.datesContainer}>
-      <Text style={styles.date}>03-05</Text>
-      <Text style={styles.month}>NOV</Text>
-      </View>
-      <View style={{flex: 1}}>
-      <Text style={styles.round}>Round 21</Text>
-      <Text style={styles.country}>{item.competition.location.country}</Text>
-      <Text style={styles.description}>
-        {item.competition.name} {item.season}
-      </Text>
-      </View>
-      <Entypo name="chevron-right" size={24} color={Colors.primary} />
-    </View>
-  )
-}
+import RaceListItem from './src/Constants/components/RaceListItem';
 
 export default function App() {
   return (
@@ -32,7 +12,7 @@ export default function App() {
       <FlatList 
        data={races}
        renderItem={({item}) => (
-         <RaceListItem item={item} />
+         <RaceListItem item={item} round={0} />
        )}
       />
       <StatusBar style="auto" />
@@ -46,50 +26,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'whitesmoke',
 
   },
-  itemContainer: {
-    padding: 10,
-    margin: 5,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-	  width: 0,
-	  height: 1,
-  },
-    shadowOpacity: 0.18,
-    shadowRadius: 1.00,
-    elevation: 1,
-  },
-  date: {
-
-  },
-  month: {
-    backgroundColor: 'gainsboro',
-    paddingVertical: 3,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-    overflow: 'hidden',
-    color: 'dimgray',
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  round: {
-    color: Colors.primary,
-  },
-  country:{
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  description: {
-    color: 'dimgray'
-  },
-  datesContainer: {
-    padding: 10,
-    marginRight: 10,
-    borderRightWidth: 1,
-    borderColor: 'gainsboro',
-    alignItems: 'center',
-  }
+  
 });
